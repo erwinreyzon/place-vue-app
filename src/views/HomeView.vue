6e42cbcf2eab2b30c38d93839c54a311d18b0a5b
erwin <winzon353535@gmail.com>
2022-04-12 14:52:25 -0700

    adds index to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Places",
+      places: [],
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/places.json").then((response) => {
+      this.places = response.data;
+      console.log("All Places", this.places);
+    });
+  },
+  methods: {},
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+
+    <div v-for="place in places" v-bind:key="place.id">
+      <p>{{ place.name }}</p>
+      <p>{{ place.address }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style></style>
